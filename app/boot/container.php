60cbfa856203b9b5fcfa4ac8c0d2140c61f0e142
Ian Grindley <ian@ignition-studios.com>
2019-12-13 12:39:23 +0000

    Setup feed views and helper views in app namespace
    Added create and form view - form.twig will be shared between create/edit views
    Added supporting includes into App namespace, alerts
    Added todo.md into theme folder to aid memory..
    Updated container to support form_protect() csrf function if required

--- app/boot/container.php
+++ app/boot/container.php
@@ -2,6 +2,12 @@
 /*
  *	Setup your container dependencies here
  */
+// set csrf protection
+$container['csrf'] = function ($c) {
+	$csrf = new \Slim\Csrf\Guard;
+	$csrf->setPersistentTokenMode(true);
+	return $csrf;
+};
 // app logger
 $container['log'] = function($c) {
 	$log = new \Monolog\Logger('app');
@@ -11,6 +17,7 @@ $container['log'] = function($c) {
 // set renderer
 $container['view'] = function($c) {
 	$view = new \Slim\Views\Twig([
+		'app' => path('app/src/App/views'),
 		'feeds' => path('app/src/Feeds/views'),
 	], [
 		'cache' => path('storage/views'),
@@ -20,5 +27,12 @@ $container['view'] = function($c) {
 	// Instantiate and add Slim specific extension
 	$uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
 	$view->addExtension(new Slim\Views\TwigExtension($c->get('router'), $uri));
+	// Add form_protect function which integrates with Slim/Csrf
+	$view->getEnvironment()->addFunction(new \Twig\TwigFunction('form_protect', function() use($c) {
+		$csrf = $c->get('csrf');
+		$out = sprintf('<input type="hidden" id="_token_name" name="%s" value="%s">', $csrf->getTokenNameKey(), $csrf->getTokenName());
+		$out.= sprintf('<input type="hidden" id="_token_value" name="%s" value="%s">', $csrf->getTokenValueKey(), $csrf->getTokenValue());
+		return $out;
+	}));
 	return $view;
 };
---
+++ public/themes/todo.md
@@ -0,0 +1,5 @@
+Add buttons, btn-link
+
+JS:
+On edit - save button disabled
+On form fields changed - save button enabled
